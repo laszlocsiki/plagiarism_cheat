d7b8d773604c4a3edc1cc29eb560e45d50f1672f
Csiki Laszlo <laszlo.csiki@elearningsoftware.ro>
2017-01-12 11:04:09 +0200

    add java project

---
+++ Java/Plagiarism/src/main/java/plagiarism/App.java
@@ -0,0 +1,13 @@
+package plagiarism;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
